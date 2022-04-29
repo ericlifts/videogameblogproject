0f3fc6696ce2f73afe38d984beb39936c64c23e3
ericlifts <aragon.eric96@gmail.com>
2022-04-28 20:18:09 -0700

    showing posts

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class BlogController extends Controller
 {
     public function index() {
-        return view('blogPosts.blog');
+        $posts = Post::all();
+        return view('blogPosts.blog', compact('posts'));
     }
 
     public function create() {
@@ -21,11 +25,32 @@ class BlogController extends Controller
             'body' => 'required'
         ]);
 
-        dd('Validation passed.');
+        $title = $request-> input('title');
+        $slug = Str::slug($title, '-');
+        $user_id = Auth::user()-> id;
+        $body = $request-> input('body');
+
+        //to upload an image file
+        $imagePath = 'storage/' . $request-> file('image')-> store('postsImages','public');
+        // $imagePath = 'users/' . $request-> file('image')-> store('public');
+
+        $post = new Post();
+        $post-> title = $title;
+        $post-> slug = $slug;
+        $post-> user_id = $user_id;
+        $post-> body = $body;
+        $post-> imagePath = $imagePath;
+
+        $post-> save();
+
+        return redirect()-> back()-> with('status', 'Your post was created successfully!');
+
+        // dd('Validation passed.');
     }
 
-    public function show() {
-        return view('blogPosts.selected-blog-post');
+    public function show($slug) {
+        $post = Post::where('slug', $slug)-> first();
+        return view('blogPosts.selected-blog-post', compact('post'));
     }
 
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    //we have a user and a user has many post
+    public function posts(){
+        return $this-> hasMany(Post::class);
+    }
 }
